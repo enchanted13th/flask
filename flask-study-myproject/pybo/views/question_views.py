d668857095b1b73510c0c877a61869fa0c5ca69a
ine <enchanted13th@gmail.com>
2021-03-25 13:41:43 +0900

    add search & order

--- flask-study-myproject/pybo/views/question_views.py
+++ flask-study-myproject/pybo/views/question_views.py
@@ -2,33 +2,70 @@ from datetime import datetime
 
 from flask import Blueprint, render_template, request, url_for, g, flash
 from werkzeug.utils import redirect
+from sqlalchemy import func
 
 from pybo.views.auth_views import login_required
 
 from .. import db
 from ..forms import QuestionForm, AnswerForm
-from ..models import Question
+from ..models import Question, Answer, User, question_voter
 
 bp = Blueprint('question', __name__, url_prefix='/question')
 
 
 @bp.route('/list/')
 def _list():
+    # 입력 파라미터
     page = request.args.get('page', type=int, default=1)
-    question_list = Question.query.order_by(Question.create_date.desc())
+    kw = request.args.get('kw', type=str, default='')
+    so = request.args.get('so', type=str, default='recent')
+
+    # 정렬
+    if so == 'recommend':
+        sub_query = db.session.query(question_voter.c.question_id, func.count('*').label('num_voter')) \
+            .group_by(question_voter.c.question_id).subquery()
+        question_list = Question.query \
+            .outerjoin(sub_query, Question.id == sub_query.c.question_id) \
+            .order_by(sub_query.c.num_voter.desc(), Question.create_date.desc())
+    elif so == 'popular':
+        sub_query = db.session.query(Answer.question_id, func.count('*').label('num_answer')) \
+            .group_by(Answer.question_id).subquery()
+        question_list = Question.query \
+            .outerjoin(sub_query, Question.id == sub_query.c.question_id) \
+            .order_by(sub_query.c.num_answer.desc(), Question.create_date.desc())
+    else:  # recent
+        question_list = Question.query.order_by(Question.create_date.desc())
+
+    # 조회
+    if kw:
+        search = '%%{}%%'.format(kw)
+        sub_query = db.session.query(Answer.question_id, Answer.content, User.username) \
+            .join(User, Answer.user_id == User.id).subquery()
+        question_list = question_list \
+            .join(User) \
+            .outerjoin(sub_query, sub_query.c.question_id == Question.id) \
+            .filter(Question.subject.ilike(search) |  # 질문제목
+                    Question.content.ilike(search) |  # 질문내용
+                    User.username.ilike(search) |  # 질문작성자
+                    sub_query.c.content.ilike(search) |  # 답변내용
+                    sub_query.c.username.ilike(search)  # 답변작성자
+                    ) \
+            .distinct()
+
+    # 페이징
     question_list = question_list.paginate(page, per_page=10)
-    return render_template('question/question_list.html', question_list=question_list)
+    return render_template('question/question_list.html', question_list=question_list, page=page, kw=kw, so=so)
 
 
-@bp.route('/detail/<int:question_id>/')
+@ bp.route('/detail/<int:question_id>/')
 def detail(question_id):
     form = AnswerForm()
     question = Question.query.get_or_404(question_id)
     return render_template('question/question_detail.html', question=question, form=form)
 
 
-@bp.route('/create/', methods=('GET', 'POST'))
-@login_required
+@ bp.route('/create/', methods=('GET', 'POST'))
+@ login_required
 def create():
     form = QuestionForm()
     if request.method == 'POST' and form.validate_on_submit():
@@ -40,8 +77,8 @@ def create():
     return render_template('question/question_form.html', form=form)
 
 
-@bp.route('/modify/<int:question_id>', methods=('GET', 'POST'))
-@login_required
+@ bp.route('/modify/<int:question_id>', methods=('GET', 'POST'))
+@ login_required
 def modify(question_id):
     question = Question.query.get_or_404(question_id)
     if g.user != question.user:
@@ -59,8 +96,8 @@ def modify(question_id):
     return render_template('question/question_form.html', form=form)
 
 
-@bp.route('/delete/<int:question_id>')
-@login_required
+@ bp.route('/delete/<int:question_id>')
+@ login_required
 def delete(question_id):
     question = Question.query.get_or_404(question_id)
     if g.user != question.user:
